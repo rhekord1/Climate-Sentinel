0bc3d0535cb77b3058ae91f27c62bceddd2f3474
rhekord <rhekord1@gmail.com>
2025-05-05 15:10:43 +0000

    -Modified App.jsx added login and signup routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import { BrowserRouter,Routes,Route } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 export default function App() {
     return (
@@ -9,6 +11,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
         </Routes>
       </BrowserRouter>
     );
