2448ab4b1f74a4ae8dc0a652fb571943a9f6ba13
rhekord <rhekord1@gmail.com>
2025-05-09 11:11:18 +0000

    Modified App.jsx -added the dashboard page route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
     return (
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="/about" element={<About/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<Signup/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
         </Routes>
       </BrowserRouter>
     );
