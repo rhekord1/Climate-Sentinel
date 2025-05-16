aaec16afd397c19c2c101aed59f3385006ea23ca
blip <92245038+blip-cmd@users.noreply.github.com>
2025-05-16 15:58:58 +0000

    whitelist site name

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 10000 , // use Render's PORT if available
+    port: process.env.PORT || 10000 , 
+    allowedHosts: ['climate-sentinel.onrender.com'],
   }
 })
