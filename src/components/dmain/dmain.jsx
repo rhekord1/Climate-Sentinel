e977c706c0c72e514fdb658bde4f414ab72ceeea
rhekord <rhekord1@gmail.com>
2025-05-17 19:48:34 +0000

    Modified dmain.jsx -increased map minheight

--- src/components/dmain/dmain.jsx
+++ src/components/dmain/dmain.jsx
@@ -245,7 +245,7 @@ export default function MapboxMap() {
           style={{
             width: "100%",
             height: "100%",
-            minHeight: "550px"
+            minHeight: "650px"
           }}
         />
         
